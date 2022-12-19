59b3a0ca5ae09acc6261b8fac996ce7d188069f2
Nagesh Singh <nst321888@gmail.com>
2022-12-19 14:13:12 +0530

    tool tip added

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './toolTip.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <i class="fa-solid fa-yin-yang targetElement"></i>
+      </header>
+    </div>
+  );
+}
+
+export default App;
